b091198b945a574ad29afd15bbe9311110cf9894
Seraladhan1023 <seraladhan2310@gmail.com>
2026-03-05 10:20:50 +0530

    eventmangemtn page initial changes

--- src/app/components/event-page/event-page.ts
+++ src/app/components/event-page/event-page.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, ɵInternalFormsSharedModule } from "@angular/forms";
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-event-page',
   standalone: true,
   templateUrl: './event-page.html',
   styleUrl: './event-page.css',
+  imports: [ɵInternalFormsSharedModule,ReactiveFormsModule],
 })
-export class EventPage {}
+export class EventPage {
+  
+eventForm!: FormGroup;
+
+constructor(private fb: FormBuilder) {}
+
+ngOnInit() {
+  this.eventForm = this.fb.group({
+    first_name: [''],
+    last_name: [''],
+    email: [''],
+    mobile: [''],
+    event_type: [''],
+    event_date: [''],
+    city: [''],
+    budget_range: [''],
+    message: ['']
+  });
+}
+handleSubmit() {
+  console.log(this.eventForm.value); 
+ }
+
+}
