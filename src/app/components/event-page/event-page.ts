440d39aa27a62ce7883149e9dc7040b0caa973d0
Seraladhan1023 <seraladhan2310@gmail.com>
2026-03-06 14:34:38 +0530

    event-management for changes

--- src/app/components/event-page/event-page.ts
+++ src/app/components/event-page/event-page.ts
@@ -1,35 +1,57 @@
 import { Component } from '@angular/core';
-import { FormGroup, ɵInternalFormsSharedModule } from "@angular/forms";
+import { FormGroup, ɵInternalFormsSharedModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
+import { Events } from '../../services/events';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef } from '@angular/core';
 @Component({
   selector: 'app-event-page',
   standalone: true,
   templateUrl: './event-page.html',
   styleUrl: './event-page.css',
-  imports: [ɵInternalFormsSharedModule,ReactiveFormsModule],
+  imports: [ɵInternalFormsSharedModule, ReactiveFormsModule,CommonModule],
 })
 export class EventPage {
-  
-eventForm!: FormGroup;
+  eventForm!: FormGroup;
+  successMessage= false;
+  constructor(private fb: FormBuilder,private eventsService: Events,private cdr:ChangeDetectorRef) {}
+  ngOnInit() {
+    this.eventForm = this.fb.group({
+      first_name: [''],
+      last_name: [''],
+      email: [''],
+      mobile: [''],
+      event_type: [''],
+      event_date: [''],
+      city: [''],
+      budget_range: [''],
+      message: [''],
+    });
+  }
 
-constructor(private fb: FormBuilder) {}
+  handleSubmit() {
+    const eventData = this.eventForm.value;
+    console.log(this.eventForm.value);
 
-ngOnInit() {
-  this.eventForm = this.fb.group({
-    first_name: [''],
-    last_name: [''],
-    email: [''],
-    mobile: [''],
-    event_type: [''],
-    event_date: [''],
-    city: [''],
-    budget_range: [''],
-    message: ['']
-  });
-}
-handleSubmit() {
-  console.log(this.eventForm.value); 
- }
+    this.eventsService.createEvent(eventData).subscribe(
+      (response) => {
+        console.log('Event created successfully:', response);
+        this.successMessage = true;
+        setTimeout(() => {
+          this.successMessage = false;
+          console.log('Success message cleared');
+        }, 3000); 
+        this.cdr.detectChanges();
+        this.eventForm.reset();
+      },
+      (error) => {
+        console.error('Error creating event:', error);
+      }
+    );
+  }
 
+  closePopup(){
+    this.successMessage = false;
+  }
 }
